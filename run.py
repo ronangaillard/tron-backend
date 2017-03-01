89dd49171605067a5f76db4114e052cac207f500
Ronan Gaillard <ronan.gaillard@live.fr>
2017-03-01 15:48:39 +0100

    Update t osupport lighttpd

--- run.py
+++ run.py
@@ -1,24 +1,21 @@
 from mainapp import app
 from flask_cors import CORS, cross_origin
 
-print 'Game backend by Ronan'
+if __name__ == '__main__':
+    try:
+        # Development server
+        open('./DEVELOPMENT').close()
 
+        print "WARNING RUNNING AS DEVELOPMENT SERVER"
+        app.config.from_object('configuration.BaseConfig')
+        CORS(app, supports_credentials = True)
+        app.run(host='0.0.0.0')
 
-
-try:
-    # Development server
-    open('./DEVELOPMENT').close()
-
-    print "WARNING RUNNING AS DEVELOPMENT SERVER"
-    app.config.from_object('configuration.BaseConfig')
-    CORS(app, supports_credentials = True)
-    app.run(host='0.0.0.0')
-
-except:
-    # Production server
-    print "Running as production server"
-    app.config.from_object('configuration.ProdConfig')
-    CORS(app, supports_credentials = True)
-    app.run(host='0.0.0.0')
+    except:
+        # Production server
+        print "Running as production server"
+        app.config.from_object('configuration.ProdConfig')
+        CORS(app, supports_credentials = True)
+        app.run(host='0.0.0.0')
 
 
